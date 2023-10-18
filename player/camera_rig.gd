c54a1b5353e9ea7c2d09b67bdb676e468802c0b3
Adrian <adriancoliveira@yahoo.com>
2023-10-18 15:36:23 -0300

    Mudanças no shader e no cenário relacionadas a camera

--- player/camera_rig.gd
+++ player/camera_rig.gd
@@ -7,14 +7,17 @@ extends Node3D
 @onready var foreground_camera = $BaseCamera/ForegroundViewportContainer/ForegroundViewport/ForegroundCamera
 
 
+## Ignore o codigo abaixo, ele provavelmente é inútil
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	resize()
+#func _ready():
+#	resize()
 
-func resize():
-	background_viewport.size = DisplayServer.window_get_size()
-	foreground_viewport.size = DisplayServer.window_get_size()
+#func resize():
+#	background_viewport.size = DisplayServer.window_get_size()
+#	foreground_viewport.size = DisplayServer.window_get_size()
+
+## Fim da parte inútil, ela ta ai só pro caso de acontecer alguma coisa
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
