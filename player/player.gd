e6d21b4db93b6aa1d22ff530c6e8b4ad90a945ae
Adrian <adriancoliveira@yahoo.com>
2023-10-25 18:01:49 -0300

    Animation Tree do jogador adicionado

--- player/player.gd
+++ player/player.gd
@@ -23,6 +23,15 @@ func _ready():
 	state_machine = animation_tree.get("parameters/playback")
 
 func _physics_process(delta):
+	
+	match state_machine:
+		"idle":
+			pass
+		"walk":
+			pass
+		"mma_kick":
+			pass
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -30,6 +39,9 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_just_pressed("attack"):
+		state_machine.travel("mma_kick")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -40,22 +52,16 @@ func _physics_process(delta):
 		velocity.z = direction.z * SPEED
 		
 		visuals.look_at(direction + position)
-		
-#		if !walking:
-#			walking = true
-#			animation_player.play("walk")
+	
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-		
-#		if walking:
-#			walking = false
-#			animation_player.play("idle")
 	
-	animation_tree.set("parameters/conditions/idle", !is_player_walking())
-	animation_tree.set("parameters/conditions/walk", is_player_walking())
+	
+	animation_tree.set("parameters/conditions/idle", !_is_player_walking())
+	animation_tree.set("parameters/conditions/walk", _is_player_walking())
 	
 	move_and_slide()
 
-func is_player_walking():
+func _is_player_walking():
 	return !(velocity.x == 0 and velocity.z == 0)
